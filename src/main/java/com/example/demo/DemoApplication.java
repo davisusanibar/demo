cc9120d20276be9ae2cc04a30764ef8193a0e866
david dali susanibar arce <dsusanibar@bcp.com.pe>
2018-07-01 23:56:23 -0500

    Adding rest services

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,26 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+}
+
+@RestController
+@RequestMapping
+class BCPRestController {
+	@RequestMapping(value = "/cuentas", method = RequestMethod.GET)
+	public List<String> getTipoCuenta(){
+		return Arrays.asList("Ahorros", "Corriente");
 	}
 }
